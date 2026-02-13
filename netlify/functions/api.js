527b8106f19f79e73801e89968fb2a3801f84464
asepretired <agungprama23@gmail.com>
2026-02-13 18:14:11 +0800

    Create api.js

---
+++ netlify/functions/api.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+    const { action, login, domain, id } = event.queryStringParameters;
+    
+    // Base URL 1secmail
+    let url = `https://www.1secmail.com/api/v1/?action=${action}&login=${login}&domain=${domain}`;
+    
+    if (id) {
+        url += `&id=${id}`;
+    }
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*" // Mengizinkan akses dari mana saja
+            },
+            body: JSON.stringify(data)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Gagal mengambil data dari server email" })
+        };
+    }
+};
